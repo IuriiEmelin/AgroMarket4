5ce9bb569ed4ead3f756050130f84d700d9e95cb
Gamzat <gamzik44@gmail.com>
2023-06-07 17:19:00 +0300

    Model + Login Screen

--- AgroMarket/View Controllers/LoginViewController.swift
+++ AgroMarket/View Controllers/LoginViewController.swift
@@ -9,4 +9,24 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    var users: [User] = [
+        User(username: "seller1", password: "pass1", type: .seller, company: Company(companyName: "Компания1", region: "Москва", type: .meat)),
+        User(username: "buyer1", password: "pass2", type: .buyer, company: Company(companyName: "Компания2", region: "Санкт-Петербург", type: .resale))
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        usernameField.returnKeyType = .done
+        passwordField.returnKeyType = .done
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
 }
