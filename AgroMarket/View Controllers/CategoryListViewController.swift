a437fcca1d41c4ac232191d0c1447841077da8a0
Alexey Turulin <repakuku@icloud.com>
2023-06-07 14:50:25 -0400

    added localization, redesigned segue logic

--- AgroMarket/View Controllers/CategoryListViewController.swift
+++ AgroMarket/View Controllers/CategoryListViewController.swift
@@ -12,6 +12,14 @@ final class CategoryListViewController: UITableViewController {
     var user: User?
     var products: [Product]?
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let productsListVC = segue.destination as? ProductsListViewController else { return }
+        guard let category = sender as? Category else { return }
+//        productsListVC.user = user
+//        productsListVC.products = products
+//        productsListVC.category = category
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         Category.allCases.count
     }
@@ -20,9 +28,15 @@ final class CategoryListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "сategory", for: indexPath)
         var content = cell.defaultContentConfiguration()
         
-        content.text = Category.allCases[indexPath.row].rawValue.uppercased()
+        content.text = Category.allCases[indexPath.row].rawValue
         cell.contentConfiguration = content
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowProductsScreen", sender: Category.allCases[indexPath.row])
+    }
 }
+
+
