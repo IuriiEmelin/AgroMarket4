fa09d79b741a6e55f8580207742f96ecc5480171
Gamzat <gamzik44@gmail.com>
2023-06-10 18:13:18 +0300

    About Us Screen 0.0.1

--- AgroMarket/View Controllers/TeamViewController.swift
+++ AgroMarket/View Controllers/TeamViewController.swift
@@ -9,4 +9,30 @@ import UIKit
 
 class TeamViewController: UIViewController {
 
+    @IBOutlet weak var Yuriy: UIImageView!
+    
+    @IBOutlet weak var Gamzat: UIImageView!
+    
+    @IBOutlet weak var Gordey: UIImageView!
+    
+    @IBOutlet weak var Alexey: UIImageView!
+    
+    @IBOutlet weak var Daniil: UIImageView!
+    
+    override func viewDidLoad() {
+        Yuriy.layer.cornerRadius = Yuriy.frame.width / 3
+        Yuriy.clipsToBounds = true
+        
+        Gamzat.layer.cornerRadius = Gamzat.frame.width / 3
+        Gamzat.clipsToBounds = true
+        
+        Gordey.layer.cornerRadius = Gordey.frame.width / 3
+        Gordey.clipsToBounds = true
+        
+        Alexey.layer.cornerRadius = Alexey.frame.width / 3
+        Alexey.clipsToBounds = true
+        
+        Daniil.layer.cornerRadius = Daniil.frame.width / 3
+        Daniil.clipsToBounds = true
+    }
 }
